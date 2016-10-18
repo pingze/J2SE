bb46052b0b994c56ae34fa81bbf188a5666d7ac5
pingze <1952211498@qq.com>
2016-10-18 17:19:35 +0800

    version 1.1.1

---
+++ src/com/yzp/test/Test1.java
@@ -0,0 +1,11 @@
+package com.yzp.test;
+
+/**
+ * Created by zeping.yin on 2016/8/29.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String str = "hello world";
+        System.out.print(str);
+    }
+}
